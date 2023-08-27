5a523f43551cc9034026cf6ec9da8cce2caa0dcf
aclem43 <aclemeur@gmail.com>
2023-08-27 21:12:33 +1000

    Further improvments to editor ui

--- src/pages/Note.vue
+++ src/pages/Note.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
+import { mdiAlphaH, mdiFloppy, mdiFormatBold, mdiFormatHeader1, mdiFormatHeader2, mdiFormatItalic, mdiFormatStrikethrough, mdiRedo, mdiUndo } from '@mdi/js';
 import Bold from '@tiptap/extension-bold';
 import Code from '@tiptap/extension-code';
 import Document from '@tiptap/extension-document';
+import Heading from '@tiptap/extension-heading';
 import History from '@tiptap/extension-history';
 import Italic from '@tiptap/extension-italic';
 import Link from '@tiptap/extension-link';
 import Paragraph from '@tiptap/extension-paragraph';
 import Strike from '@tiptap/extension-strike';
 import Text from '@tiptap/extension-text';
+import Typography from '@tiptap/extension-typography';
 import { BubbleMenu, EditorContent, useEditor } from '@tiptap/vue-3';
 import "../assets/editor.css";
+import Icon from '../components/Icon.vue';
+
 const editor = useEditor({
     content: '<h1> Title </h1> <p> Start typing... </p>',
     extensions: [
         Document,
+        Typography,
+        Heading,
         Paragraph,
         Text,
         Code,
@@ -28,23 +35,61 @@ const editor = useEditor({
         Strike
     ],
 })
+
+const isHeaderActive = () => {
+    if (editor.value == undefined) return false
+    return editor.value.isActive('heading', { level: 1 }) || editor.value.isActive('heading', { level: 2 })
+}
+
 </script>
 <template>
     <div class="note-container">
         <div>Note</div>
 
         <div v-if="editor != undefined" class="toolbar">
-            <button @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
-                bold
-            </button>
-            <button @click="editor.chain().focus().toggleItalic().run()"
-                :class="{ 'is-active': editor.isActive('italic') }">
-                italic
-            </button>
-            <button @click="editor.chain().focus().toggleStrike().run()"
-                :class="{ 'is-active': editor.isActive('strike') }">
-                strike
-            </button>
+            <div class="group">
+                <div class="dropdown">
+                    <button :class="{ 'is-active': isHeaderActive() }">
+                        <Icon :icon="mdiAlphaH" :size="32"></Icon>
+
+                    </button>
+                    <div class="dropdown-content">
+                        <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+                            :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
+                            <Icon :icon="mdiFormatHeader1"></Icon>
+                        </button>
+                        <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+                            :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
+                            <Icon :icon="mdiFormatHeader2"></Icon>
+                        </button>
+                    </div>
+                </div>
+                <div class="divider"></div>
+                <button @click="editor.chain().focus().toggleBold().run()"
+                    :class="{ 'is-active': editor.isActive('bold') }">
+                    <Icon :icon="mdiFormatBold"></Icon>
+                </button>
+                <button @click="editor.chain().focus().toggleItalic().run()"
+                    :class="{ 'is-active': editor.isActive('italic') }">
+                    <Icon :icon="mdiFormatItalic"></Icon>
+                </button>
+                <button @click="editor.chain().focus().toggleStrike().run()"
+                    :class="{ 'is-active': editor.isActive('strike') }">
+                    <Icon :icon="mdiFormatStrikethrough"></Icon>
+                </button>
+                <button @click="editor.chain().focus().undo().run()" :disabled="!editor.can().undo()">
+                    <Icon :icon="mdiUndo"></Icon>
+                </button>
+                <button @click="editor.chain().focus().redo().run()" :disabled="!editor.can().redo()">
+                    <Icon :icon="mdiRedo"></Icon>
+
+                </button>
+            </div>
+            <div class="group">
+                <button>
+                    <Icon :icon="mdiFloppy"></Icon>
+                </button>
+            </div>
         </div>
         <bubble-menu :editor="editor" :tippy-options="{ duration: 100 }" v-if="editor">
             <div class="mouse-overlay">
@@ -61,12 +106,7 @@ const editor = useEditor({
                     :class="{ 'is-active': editor.isActive('strike') }">
                     strike
                 </button>
-                <button @click="editor.chain().focus().undo().run()" :disabled="!editor.can().undo()">
-                    undo
-                </button>
-                <button @click="editor.chain().focus().redo().run()" :disabled="!editor.can().redo()">
-                    redo
-                </button>
+
             </div>
         </bubble-menu>
         <editor-content :editor="editor" style="flex-grow: 1;" />
