ee4481ca075171a4134cadca607d72d283ef9438
aclem43 <aclemeur@gmail.com>
2023-09-05 14:14:24 +1000

    Version 0.0.1

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+import { getVersion } from '@tauri-apps/api/app';
+import { onMounted, ref } from "vue";
 import "../assets/settings.css";
+const appVersion = ref('')
+onMounted(async () => {
+    appVersion.value = await getVersion();
 
+})
 </script>
 
 <template>
     <div class="settings-container ">
         Settings
+        <div class="settings-content">
+            <div class="settings-item">
+                <div class="settings-item-title">App Version</div>
+                <div class="settings-item-content">{{ appVersion }}</div>
+            </div>
+        </div>
 
     </div>
 </template>
+
